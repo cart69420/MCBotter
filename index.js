9eb66a32a80c1d876d26f77cd10b3f45e77a623d
cart1305 <boristheotherslave@gmail.com>
2022-01-12 13:27:02 +0700

    when bot join do what prompt

--- index.js
+++ index.js
@@ -55,11 +55,16 @@ async function getBotInfo() {
             type: 'text',
             name: 'username',
             message: 'Enter bot username (use %n for a random number from 0 to 9, %s for a random letter in the alphabet, %S for a random CAPITAL letter)\n[Leave blank for randomized username]:',
+            validate: value => 
+            !value ? true : (
+                randomName(value).length > 16 ? 'Username is too long (must be less than 16 characters)' : 
+                    (randomName(value).length < 3 ? 'Username is too short (must be at least 3 characters)' : 
+                        (!some(["%s", "%S", "%n"], value, true) ? 'Username must contain at least one of the following: %n, %s, %S' : true)))
         },
         {
             type: 'confirm',
             name: 'confirm',
-            message: (prev, values) => `Bot:\n -> Amount: ${!values.amount ? 1 : values.amount}\n -> Delay: ${!values.delay ? 5000 : values.delay}ms -> Username: ${!values.username ? "randomized" : values.username}\nConfirm?`,
+            message: (prev, values) => `Bot:\n -> Amount: ${!values.amount ? 1 : values.amount}\n -> Delay: ${!values.delay ? 5000 : values.delay}ms\n -> Username: ${!values.username ? "randomized" : values.username}\nConfirm?`,
         }
     ], {onCancel});
     
@@ -72,6 +77,87 @@ async function getBotInfo() {
     return res;
 }
 
+async function whatBotDo() {
+    var res2
+    const res = await prompts([
+        {
+            type: "select",
+            name: "action",
+            message: "What do you want the bot to do when it joins?",
+            choices: [
+                {title: "Nothing", value: "nothing"},
+                {title: "Send message", value: "send message"},
+                {title: "Spam message", value: "spam message"},
+                {title: "DM Others", value: "dm others"},
+                {title: "Spam DM", value: "spam dm"}
+            ],
+        }
+    ], {onCancel});
+
+    if (res.action == "spam message" || res.action == "spam dm") {
+        res2 = await prompts([
+            {
+                type: "text",
+                name: "message",
+                message: "Enter the message to spam [Leave blank for default message (advertise)]:",
+            },
+            {
+                type: "number",
+                name: "times",
+                message: "Enter the amount of times to spam [Leave blank for infinite]:",
+            },
+            {
+                type: "number",
+                name: "delay",
+                message: "Enter the delay between each message (in miliseconds) [Leave blank for the default delay (1000ms)]:",
+            },
+            {
+                type: "confirm",
+                name: "confirm",
+                message: (prev, values) => `Action: (${res.action == "spam message" ? "Spam Message" : "Spam DMs"}):\n -> Message: ${!values.message ? "advertise" : values.message}\n -> Times: ${!values.times ? "infinite" : values.times}\n -> Delay: ${!values.delay ? 1000 : values.delay}ms\nConfirm?`,
+            }
+        ], {onCancel})
+    } else if (res.action == "dm others" || res.action == "send message") {
+        res2 = await prompts([
+            {
+                type: "text",
+                name: "message",
+                message: "Enter the message to send [Leave blank for default message (advertise)]:",
+            },
+            {
+                type: "confirm",
+                name: "confirm",
+                message: (prev, values) => `Action: ${res.action == "send message" ? "Send Message" : "DM Others"}:\n -> Message: ${!values.message ? "advertise" : values.message}\nConfirm?`,
+            }
+        ], {onCancel});
+    } else {
+        res2 = await prompts([
+            {
+                type: "confirm",
+                name: "confirm",
+                message: (prev, values) => `Action: ${res.action == "nothing" ? "Nothing" : "Nothing"}:\nConfirm?`,
+            }
+        ], {onCancel});
+    }
+
+    if (!res2.confirm) return whatBotDo();
+
+    if (res2) {
+        if (!res2.message) res2.message = "mcbotter is swag";
+        if (!res2.times) res2.times = Infinity;
+        if (!res2.delay) res2.delay = 1000;
+    }
+
+    return {res:res, res2:res2}
+}
+
+function some(array, value, includes = false) {
+    for (var i = 0; i < array.length; i++) {
+        if (includes ? value.includes(array[i]) : array[i] == value) return true
+    }
+    return false;
+}
+
 function isVersion(version) {
     if (!(new RegExp("^([0-9]+).([0-9]+).([0-9]+)$").test(version))) {
         return true
@@ -80,8 +166,8 @@ function isVersion(version) {
     } else return false
 }
 
-function randomName() {
-    let username = process.botter.bot.username
+function randomName(name) {
+    let username = name
     if (username == 'random') {
         username = ''
         let list = ["%n", "%s", "%S"]
@@ -99,20 +185,28 @@ function randomName() {
     return username
 }
 
-async function sendBot() {
+async function sendBot(username) {
     let n = 0;
     for (var i = 0; i < process.botter.bot.amount; i++) {
         setTimeout(async() => {
             n++
-            await console.log("Sending bot " + n + " of " + process.botter.bot.amount + " > " + randomName());
+            let name = randomName(username)
+            if (process.botter.botlist.includes(name)) console.log(`${name} has already logged in, skipping... (${n}/${process.botter.bot.amount})`)
+            else {
+                await console.log(`Sending ${name} (${n}/${process.botter.bot.amount})`);
+                process.botter.botlist.push(name)
+            }
         }, i * process.botter.bot.delay);
     }
 }
 
 getServerInfo().then(resinfo => {
     getBotInfo().then(resbot => {
-        process.botter = {server: resinfo, bot: resbot};
-        sendBot();
+        process.botter = {server: resinfo, bot: resbot, botlist: []};
+        whatBotDo().then(resaction => {
+            sendBot(resbot.username);
+            console.log(resaction)
+        })
     })
 });
 
